8f445ae31aeb4ea79f856ede44b45ea329143955
Rohith <r.k.reddy180802@gmail.com>
2022-12-28 14:46:58 +0530

    modified connection.py

--- connection.py
+++ connection.py
@@ -1,52 +1,41 @@
 import socket
 from protocol import protocol
-import sys 
+import sys
+
 
 class connection:
 
-    def __init__(self,host,port):
-        self.host=host
-        self.port=port
-
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
     def connectSock(self):
         print("CONNECTING TO PLUTO.....")
         try:
-            sockID=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        except socket.TimeoutError:
+            sockID = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.timeout:
             self.connectSock()
-            
+
         except socket.InterruptedError:
             print("Interrupted..Trying again")
             self.connectSock()
-        
+
         try:
-            sockID.connect((self.host,self.port))
-        except socket.error:
-            print("Connection error , Terminating program\n")
-            sys.exit(1)
+            sockID.connect((self.host, self.port))
         except socket.gaierror:
             print("issue in address,Terminating program\n")
             sys.exit(1)
+        except socket.error:
+            print("Connection error , Terminating program\n")
+            sys.exit(1)
 
         return sockID
-            
-            
-        
+
     def multiSock(self):
         print("CONNECTING TO PLUTOS....")
 
-        socket_list=list()
+        socket_list = list()
         for i in range(2):
-            sock_app=self.connectSock()
+            sock_app = self.connectSock()
             socket_list.append(sock_app)
         return socket_list
-        
-    
-
-
-
-
-
-
-
